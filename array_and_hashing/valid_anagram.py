276275e03874b5210ea97d74a1f36634a3e9e6c2
Eduardo Palhares <xeduardopalhares@gmail.com>
2024-06-20 18:40:42 -0300

    add is anagram solution

---
+++ array_and_hashing/valid_anagram.py
@@ -0,0 +1,21 @@
+def count_string(string):
+    hashmap = {}
+    for c in string:
+        hashmap[c] = hashmap.get(c, 0) + 1
+    return hashmap
+
+def is_anagram(s, t):
+    if len(s) != len(t):
+        return False
+
+    s_hashmap = count_string(s)
+    t_hashmap = count_string(t)
+
+    for k, v, in s_hashmap.items():
+        if t_hashmap.get(k, -1) != v:
+            return False
+    
+    return True
+
+def is_anagram_2(s, t):
+    return sorted(s) == sorted(t)
