46e1571535d676221d5d57aeea991370432350c8
Eduardo Palhares <xeduardopalhares@gmail.com>
2024-07-12 11:22:22 -0300

    add product of except self

---
+++ array_and_hashing/product_of_except_self.py
@@ -0,0 +1,20 @@
+def product_except_self(nums):
+    res = [1] * (len(nums))
+
+    prefix = 1
+
+    for i in range(len(nums)):
+        res[i] = prefix
+        prefix *= nums[i]
+
+    postfix = 1
+    for i in range(len(nums) - 1, -1, -1):
+        res[i] *= postfix
+        postfix *= nums[i]
+
+    return res
+
+
+if __name__ == "__main__":
+    print(product_except_self([1,2,3,4]))
+            
