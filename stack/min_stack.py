cf0e3348e7fbda7f8cdb1f164d9a8abcec9303fc
Eduardo Palhares <xeduardopalhares@gmail.com>
2024-07-30 18:51:00 -0300

    -m add min stack

---
+++ stack/min_stack.py
@@ -0,0 +1,19 @@
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append([val, self.min])
+        min_val = min(val, self.min_stack[-1] if self.min_stack else val)
+        self.min_stack.append(min_val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.min_stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def get_min(self) -> int:
+        return self.min_stack[-1]
